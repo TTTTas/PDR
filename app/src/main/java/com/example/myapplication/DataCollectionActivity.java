0e773c9528d32cc0f01b2616b8dd188caa2c5d8c
Yuyyyuuu <2021302141220@whu.edu.cn>
2024-03-17 11:29:15 +0800

    新建了两个界面，原来的mainactivity打算作为主界面，然后通过其内的两个按钮分别跳转到数据采集界面和实时PDR界面。

---
+++ app/src/main/java/com/example/myapplication/DataCollectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataCollectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_collection);
+    }
+}
